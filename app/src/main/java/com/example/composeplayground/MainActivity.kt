2b4cb057692f616ebd45e9c455d892e515e316c8
Arturo Kuang <akuang@wyze.com>
2022-11-27 01:26:15 -0800

    add circle icon to avatar, update build.gradle to java 11

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,28 +3,25 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,8 +37,8 @@ class MainActivity : ComponentActivity() {
                     CourseHeader(
                         imageUrl = "https://media.istockphoto.com/id/991883866/photo/stylish-young-man-in-yellow-hoodie-and-white-pants-sitting-on-chair-and-looking-away-on-white.jpg?s=170667a&w=0&k=20&c=nzKdmXK7ce63VoyPIHXpBAaVIBKuqnxph37Z9zJadNY=",
                         imageTitle = "Dimest C.",
-                        imageDescription = "Read 240 consecutive pages")
-
+                        imageDescription = "Read 240 consecutive pages"
+                    )
                 }
             }
         }
@@ -55,26 +52,66 @@ fun CourseHeader(
     imageDescription: String,
     modifier: Modifier = Modifier
 ) {
-    Row() {
+    Row(modifier = modifier) {
+        CourseHeaderAvatar(imageUrl = imageUrl, modifier = modifier)
+    }
+}
+
+@Composable
+fun CourseHeaderAvatar(
+    imageUrl: String,
+    modifier: Modifier
+) {
+    Box(modifier) {
         AsyncImage(
             model = imageUrl,
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier
+            modifier = modifier
                 .clip(RoundedCornerShape(12.dp))
                 .size(80.dp)
                 .aspectRatio(1f)
         )
+        Icon(
+            painter = Circle(Color.Blue),
+            contentDescription = "icon status",
+            modifier = Modifier.align(Alignment.BottomEnd).size(24.dp).padding(4.dp),
+            tint = Color.Unspecified
+        )
     }
-
 }
 
+class Circle(private val color: Color) : Painter() {
+    override val intrinsicSize: Size
+        get() = Size(10f, 10f)
 
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
+    override fun DrawScope.onDraw() {
+        val stroke = Stroke(2.5f)
+        drawCircle(color = color)
+        val arcRadius = (size.minDimension / 2f) - (stroke.width * 2f)
+        val arcBounds = Rect(
+            size.center.x - arcRadius,
+            size.center.y - arcRadius,
+            size.center.x + arcRadius,
+            size.center.y + arcRadius
+        )
+        drawArc(
+            color = Color.White,
+            startAngle = 0f,
+            sweepAngle = 360f,
+            useCenter = false,
+            alpha = .6f,
+            topLeft = arcBounds.topLeft,
+            size = arcBounds.size,
+            style = stroke
+        )
+    }
+}
+
+// @Preview(showBackground = true)
+// @Composable
+// fun DefaultPreview() {
 //    ComposePlaygroundTheme {
 //
 //    }
-//}
+// }
