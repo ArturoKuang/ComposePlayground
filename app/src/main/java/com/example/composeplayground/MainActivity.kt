1f5f57b72521563b663c7b42794c3add70f72c5e
Arturo Kuang <akuang@wyze.com>
2022-12-03 01:44:00 -0800

    added sheet animations, not really sure why it's slow on sony phone. perhaps a debug build issue?

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,6 +24,7 @@ import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
@@ -33,14 +39,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    CourseHeader(
-                        imageUrl = "https://media.istockphoto.com/id/991883866/photo/stylish-young-man-in-yellow-hoodie-and-white-pants-sitting-on-chair-and-looking-away-on-white.jpg?s=170667a&w=0&k=20&c=nzKdmXK7ce63VoyPIHXpBAaVIBKuqnxph37Z9zJadNY=",
-                        imageTitle = "Dimest C.",
-                        imageDescription = "Read 240 consecutive pages",
-                        modifier = Modifier.padding(start = 16.dp, top = 32.dp)
-                    )
 
-                    SheetStack()
+                    Column {
+                        CourseHeader(
+                            imageUrl = "https://media.istockphoto.com/id/991883866/photo/stylish-young-man-in-yellow-hoodie-and-white-pants-sitting-on-chair-and-looking-away-on-white.jpg?s=170667a&w=0&k=20&c=nzKdmXK7ce63VoyPIHXpBAaVIBKuqnxph37Z9zJadNY=",
+                            imageTitle = "Dimest C.",
+                            imageDescription = "Read 240 consecutive pages",
+                            modifier = Modifier.padding(start = 16.dp, top = 32.dp)
+                        )
+
+                        SheetStack()
+                    }
                 }
             }
         }
@@ -49,17 +58,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SheetStack(modifier: Modifier = Modifier) {
-    TopSheet(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 150.dp)
+    val sheetState = remember { mutableStateOf(SheetState.Collapsed) }
+
+    val heightTop: Dp by animateDpAsState(
+        targetValue = when (sheetState.value) {
+            SheetState.Collapsed -> 100.dp
+            SheetState.Expanded -> 20.dp
+        }
     )
 
-    BottomSheet(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 268.dp)
+    val heightBottom: Dp by animateDpAsState(
+        targetValue = when (sheetState.value) {
+            SheetState.Collapsed -> 300.dp
+            SheetState.Expanded -> 500.dp
+        }
     )
+
+    Box(modifier = modifier) {
+        TopSheet(
+            modifier = Modifier
+                .padding(top = heightTop)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .clickable {
+                    sheetState.value = when (sheetState.value) {
+                        SheetState.Expanded -> SheetState.Collapsed
+                        SheetState.Collapsed -> SheetState.Expanded
+                    }
+                }
+        )
+
+        BottomSheet(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(top = heightBottom)
+                .clickable {
+
+                }
+        )
+    }
 }
 
 @Composable
@@ -73,6 +111,7 @@ fun BottomSheet(
             .background(MaterialTheme.colors.onPrimary)
             .padding(16.dp)
     ) {
+
     }
 }
 
@@ -192,6 +231,11 @@ fun CourseHeaderAvatar(
             tint = Color.Unspecified
         )
     }
+}
+
+enum class SheetState {
+    Collapsed,
+    Expanded
 }
 
 class Circle(private val color: Color) : Painter() {
